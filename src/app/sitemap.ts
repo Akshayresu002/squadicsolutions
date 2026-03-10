02c70271efe3f67b8eddeea998994315aaffa7f1
Akshayresu002 <akshayextraleads@gmail.com>
2026-03-10 20:59:03 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = 'https://squadicsolutions.store';
+    const baseUrl = 'https://squadicsolutions.online';
 
     // Base routes
     const routes = [
